23ffb32dbf5adbbabe4a2eb981205f3a6dd27229
Nguyễn Gia Hưng <giahungandfriends@gmail.com>
2026-01-08 15:08:36 +0700

    add tag

--- api/main.py
+++ api/main.py
@@ -1,27 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from sqlalchemy.orm import Session
 from starlette import status
 
-from .schemas.schemas import Text,Response
+from schemas.schemas import Text,Response
 from models.model import TextModel,ResponseModel
+from database import get_db
 app = FastAPI()
 
-@app.post("/input/{id}",status_code=status.HTTP_201_CRREATED)
-def create(request: Text, db:Session):
+@app.post("/input",status_code=status.HTTP_201_CREATED,tags=["request"])
+def create(request: Text, db:Session=Depends(get_db)):
     put=TextModel(body=request.body)
     db.add(put)
     db.commit()
     db.refresh(put)
     return put
-@app.get("/response",status_code=status.HTTP_200_OK)
-def response(response:Response):
-    #logic here
+@app.get("/response",status_code=status.HTTP_200_OK,tags=["response"])
+def response1(response:Response):
+    response.body="idk"
     return response
-@app.get("/get input/{id}",status_code=status.HTTP_200_OK)
-def get_input(id:int,db:Session):
+@app.get("/get_input/{id}",status_code=status.HTTP_200_OK,tags=["request"])
+def get_input(id:int,db:Session=Depends(get_db)):
     get=db.query(TextModel).filter(TextModel.id==id).first()
     return get
-@app.get("/get_response/{id}",status_code=status.HTTP_200_OK)
-def response(id:int,db:Session):
+@app.get("/get_response/{id}",status_code=status.HTTP_200_OK,tags=["response"])
+def response2(id:int,db:Session=Depends(get_db)):
     get=db.query(ResponseModel).filter(TextModel.id==id).first()
     return get
--- api/models/model.py
+++ api/models/model.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String,ForeignKey
-from ..database import Base
-
+from database import Base,engine
 class TextModel(Base):
     __tablename__="texts"
     id = Column(Integer,primary_key=True,index=True)
@@ -9,3 +8,4 @@ class ResponseModel(Base):
     __tablename__="responses"
     id =Column(Integer,primary_key=True,index=True)
     body = Column(String)
+Base.metadata.create_all(engine)
